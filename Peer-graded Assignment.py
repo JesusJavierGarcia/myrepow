7a8b21499f54b94cca8bcdae704a9bd3a2f3ed09
JesusJavierGarcia <108591508+JesusJavierGarcia@users.noreply.github.com>
2022-07-11 20:46:46 -0600

    Peer graded assignment

---
+++ Peer-graded Assignment.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Jesus Javier Garcia**
+
+# My desired occupation is Data Scientist.
+
+# _I am Interested in Data Science because I like that when you master the tools that are required to apply it you are able to analyze big chunks of data in a way most people cannot_
+
+# ### My code asks the user which is her/his favorite ice cream and prints the answer back
+
+# In[1]:
+
+
+favorite_icecream = input("Which is your favorite ice cream flavour?")
+print(favorite_icecream)
+
+
+# ![1218214.jpg](attachment:1218214.jpg)
+# Bulleted list: 
+# - 1
+# - 2
+# - 3
+# Numbered lsit:
+# 1. One
+# 2. Two
+# 3. Three
+# 
+# 
